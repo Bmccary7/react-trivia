3efc9958cde74d13fec98e1d03f54f0d0a629a85
Brandon McCary <mccarymid7@gmail.com>
2024-05-10 10:00:45 -0500

    Card template formed

--- react-trivia-project-bmccary/src/App.jsx
+++ react-trivia-project-bmccary/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="titleBox">
+        <h1 className="name">Brandon McCary</h1>
+        <div className="cardBackground">
+          <div className="card">
+            <div className="logoContainer">
+              <img src={viteLogo} className="logo vite" alt="Vite logo" />
+              <h2 className="title">Silly trivia with qwazy questions!</h2>
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </div>
+            <div className="contextBox">context for me</div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
